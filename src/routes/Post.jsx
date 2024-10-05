90d30d9b0c9a45f928ab26fc4f18fdabb0666931
priyankaTtk <120715630+priyankaTtk@users.noreply.github.com>
2024-10-05 13:09:40 +0530

    Add files via upload

---
+++ src/routes/Post.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../App.css';
+
+function Post() {
+  return (
+    <div className="popup-container"> {/* Centers the Post content */}
+      <h1>Just Another Movie Website</h1>
+      <p>Welcome to the Post page. Here you can find various posts related to movies.</p>
+    </div>
+  );
+}
+
+export default Post;
